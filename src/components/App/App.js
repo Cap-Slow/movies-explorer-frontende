d36f379ba5190e40ee7ceed4dfb2ac75c4d6c292
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-29 22:51:21 +0400

    feat: add routes for SavedMovies and Profile components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -15,10 +17,26 @@ function App() {
         }
       />
       <Route
-        path="movies"
+        path="/movies"
         element={
           <div className="page">
             <Movies></Movies>
+          </div>
+        }
+      />
+      <Route
+        path="/saved-movies"
+        element={
+          <div className="page">
+            <SavedMovies></SavedMovies>
+          </div>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <div className="page">
+            <Profile></Profile>
           </div>
         }
       />
