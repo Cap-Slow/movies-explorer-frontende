bbc7af7e4336d194fda130d533b7368e65b3cbfb
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-29 20:27:55 +0400

    feat: add navigation to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import logoPath from '../../images/circle-logo.svg';
+import { useNavigate } from 'react-router-dom';
 
 function Header({ children, isLoggedIn }) {
+  const navigate = useNavigate();
   return (
     <header className={`header ${isLoggedIn ? '' : 'header_theme_pink'}`}>
       <div className="header__container">
         <img
+          onClick={() => navigate('/')}
           src={logoPath}
           alt="Логотип дипломного проекта"
           className="header__logo"
