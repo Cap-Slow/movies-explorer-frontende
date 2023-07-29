b17d1b0ecc9435d0527be9817beafaf4602a8609
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-29 20:27:22 +0400

    feat: add Preloader and MoviesCardList component into Movies component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,5 +1,9 @@
 import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../Preloader/Preloader';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+
+import Footer from '../Footer/Footer';
 
 function Movies() {
   return (
@@ -17,6 +21,9 @@ function Movies() {
         </div>
       </Header>
       <SearchForm />
+      {/* <Preloader /> */}
+      <MoviesCardList />
+      <Footer />
     </section>
   );
 }
