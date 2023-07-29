199fef2d645a9b5ec6b5cfc5c503f046d0c64255
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-29 17:18:15 +0400

    add new styles for Header component. Add isLoggedIn props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 import logoPath from '../../images/circle-logo.svg';
 
-function Header({ children }) {
+function Header({ children, isLoggedIn }) {
   return (
-    <header className="header">
+    <header className={`header ${isLoggedIn ? '' : 'header_theme_pink'}`}>
       <div className="header__container">
         <img
           src={logoPath}
