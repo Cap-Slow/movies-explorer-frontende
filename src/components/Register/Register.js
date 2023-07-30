7d281bc781b71e5e1374ccec4736746d429a89c3
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-30 17:39:48 +0400

    feat: remove a piece of common logic

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,34 +1,27 @@
-import logoPath from '../../images/circle-logo.svg';
+import AuthPage from '../AuthPage/AuthPage';
 import FormInput from '../FormInput/FormInput';
 import InputError from '../InputError/InputError';
 
 function Register() {
   return (
     <section className="register">
-      <div className="register__container">
-        <img className="register__logo" src={logoPath} alt="Логотип проекта" />
-        <h2 className="register__title">Добро пожаловать!</h2>
-        <form className="register__form">
-          <label className="register__label">Имя</label>
-          <FormInput type="text" required />
-          <InputError />
-          <label className="register__label">E-mail</label>
-          <FormInput type="email" required />
-          <InputError />
-          <label className="register__label">Пароль</label>
-          <FormInput type="password" required />
-          <InputError>Что-то пошло не так...</InputError>
-          <button className="register__submit-button" type="submit">
-            Зарегистрироваться
-          </button>
-          <p className="register__text">
-            Уже зарегистрированы?{' '}
-            <a className="register__link" href="/signin">
-              Войти
-            </a>
-          </p>
-        </form>
-      </div>
+      <AuthPage title={'Добро пожаловать!'}>
+        <FormInput type="text" required labelName={'Имя'} />
+        <InputError />
+        <FormInput type="email" required labelName={'E-mail'} />
+        <InputError />
+        <FormInput type="password" required labelName={'Пароль'} />
+        <InputError>Что-то пошло не так...</InputError>
+        <button className="register__submit-button" type="submit">
+          Зарегистрироваться
+        </button>
+        <p className="register__text">
+          Уже зарегистрированы?{' '}
+          <a className="register__link" href="/signin">
+            Войти
+          </a>
+        </p>
+      </AuthPage>
     </section>
   );
 }
