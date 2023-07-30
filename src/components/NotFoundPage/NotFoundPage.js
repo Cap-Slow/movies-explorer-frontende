98346fc0b6357bf806838620a64d3eb9df560924
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-30 14:23:06 +0400

    feat: add NotFoundPage component

---
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,11 @@
+function NotFoundPage() {
+  return (
+    <div className="not-found-page">
+      <h2 className="not-found-page__title">404</h2>
+      <p className="not-found-page__text">Страница не найдена</p>
+      <button className="not-found-page__back-button">Назад</button>
+    </div>
+  );
+}
+
+export default NotFoundPage;
