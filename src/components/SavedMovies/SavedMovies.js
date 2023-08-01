5890b5cf126881b1b572346fad9d72d48dbe670c
Dmitrii Liadov <d.liadov@xsolla.com>
2023-08-01 21:44:23 +0400

    feat: add media queries for 768 width

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,4 +1,5 @@
 import Header from '../Header/Header';
+import Navigation from '../Navigation/Navigation';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import MoviesCard from '../MoviesCard/MoviesCard';
@@ -11,16 +12,7 @@ function SavedMovies() {
   return (
     <section className="saved-movies">
       <Header isLoggedIn={true}>
-        <div className="header__nav-container">
-          <a className="header__link header__link_font_medium">Фильмы</a>
-          <a className="header__link header__link_font_regular header__link_margin_low">
-            Сохранённые фильмы
-          </a>
-          <a className="header__link header__link_font-size_regular header__link_margin_medium">
-            Аккаунт
-          </a>
-          <button className="header__account-button header__button"></button>
-        </div>
+        <Navigation />
       </Header>
       <SearchForm />
       <MoviesCardList isMoviesPage={false}>
