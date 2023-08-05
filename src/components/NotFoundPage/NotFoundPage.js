f9ebb73c2f19384bf3e5de3dd48ce6d365b479c8
Dmitrii Liadov <d.liadov@xsolla.com>
2023-08-05 22:26:01 +0400

    feat: add navigation to the previous page

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,9 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
 function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="not-found-page">
       <h2 className="not-found-page__title">404</h2>
       <p className="not-found-page__text">Страница не найдена</p>
-      <button className="not-found-page__back-button">Назад</button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+        className="not-found-page__back-button"
+      >
+        Назад
+      </button>
     </div>
   );
 }
