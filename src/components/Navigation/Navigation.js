9df044039fa2d5b93575b2a4633709148c5299cb
Dmitrii Liadov <d.liadov@xsolla.com>
2023-08-05 17:50:40 +0400

    feat: add onClick attribute for the open menu button

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-function Navigation() {
+function Navigation({ onMenuOpen }) {
   return (
     <nav className="navigation__container">
       <a className="navigation__link navigation__link_font_medium">Фильмы</a>
@@ -8,7 +8,10 @@ function Navigation() {
       <a className="navigation__link navigation__link_font-size_regular navigation__link_margin_medium">
         Аккаунт
       </a>
-      <button className="navigation__account-button navigation__button"></button>
+      <button
+        onClick={onMenuOpen}
+        className="navigation__menu-button navigation__button"
+      ></button>
     </nav>
   );
 }
