6820cc2a0c6d705771cd31fc60a1923e7e10f73f
Dmitrii Liadov <d.liadov@xsolla.com>
2023-08-05 22:21:18 +0400

    feat: use conditional rendering for buttons

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,14 @@
 import Header from '../Header/Header';
 import Navigation from '../Navigation/Navigation';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
   const [isEditableForm, setIsEditableForm] = useState(false);
   const [name, setName] = useState('Дмитрий');
   const [email, setEmail] = useState('pochta@yandex.ru');
   const [isProfileUpdateError, setIsProfileUpdateError] = useState(false);
+  const navigate = useNavigate();
 
   function editForm() {
     setIsEditableForm(true);
@@ -43,42 +45,57 @@ function Profile() {
               value={name}
               disabled={!isEditableForm}
               onChange={handleNameChange}
+              minLength="2"
             />
           </div>
           <div className="profile__row-container">
             <label className="profile__info-label">E-mail</label>
             <input
               className="profile__input"
-              type="text"
+              type="email"
               required
               value={email}
               disabled={!isEditableForm}
               onChange={handleEmailChange}
             />
           </div>
+          <p
+            className={`profile__error-message ${
+              isProfileUpdateError ? 'profile__error-message_type_visible' : ''
+            }  `}
+          >
+            При обновлении профиля произошла ошибка.
+          </p>
+          {!isEditableForm && (
+            <button
+              type="button"
+              className="profile__button "
+              onClick={editForm}
+            >
+              Редактировать
+            </button>
+          )}
+          {!isEditableForm && (
+            <button
+              type="button"
+              onClick={() => {
+                navigate('/signin');
+              }}
+              className="profile__button profile__button_color_red"
+            >
+              Выйти из аккаунта
+            </button>
+          )}
+          {isEditableForm && (
+            <button
+              type="submit"
+              className="profile__save-button"
+              onClick={handleSubmit}
+            >
+              Сохранить
+            </button>
+          )}
         </form>
-        <p
-          className={`profile__error-message ${
-            isProfileUpdateError ? 'profile__error-message_type_visible' : ''
-          }  `}
-        >
-          При обновлении профиля произошла ошибка.
-        </p>
-        {!isEditableForm && (
-          <button className="profile__button " onClick={editForm}>
-            Редактировать
-          </button>
-        )}
-        {!isEditableForm && (
-          <button className="profile__button profile__button_color_red">
-            Выйти из аккаунта
-          </button>
-        )}
-        {isEditableForm && (
-          <button className="profile__save-button" onClick={handleSubmit}>
-            Сохранить
-          </button>
-        )}
       </div>
     </section>
   );
