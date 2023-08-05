bf9b9007e50a6bc2ba86c9c4b5f9aadd9169519b
Dmitrii Liadov <d.liadov@xsolla.com>
2023-08-05 22:24:31 +0400

    fix: change ul to a tags. Add hrefs

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,20 +2,35 @@ function Portfolio() {
   return (
     <section className="portfolio" id="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
-      <ul className="portfolio__links">
-        <li className="portfolio__link-container">
+      <div className="portfolio__links">
+        <a
+          href="https://github.com/Cap-Slow/first-project"
+          target="_blank"
+          rel="noreferrer"
+          className="portfolio__link-container"
+        >
           <p className="portfolio__link-text">Статичный сайт</p>
           <button className="portfolio__link-button"></button>
-        </li>
-        <li className="portfolio__link-container">
+        </a>
+        <a
+          href="https://github.com/Cap-Slow/russian-travel"
+          target="_blank"
+          rel="noreferrer"
+          className="portfolio__link-container"
+        >
           <p className="portfolio__link-text">Адаптивный сайт</p>
           <button className="portfolio__link-button"></button>
-        </li>
-        <li className="portfolio__link-container">
+        </a>
+        <a
+          href="https://github.com/Cap-Slow/react-mesto-api-full-gha"
+          target="_blank"
+          rel="noreferrer"
+          className="portfolio__link-container"
+        >
           <p className="portfolio__link-text">Одностраничное приложение</p>
           <button className="portfolio__link-button"></button>
-        </li>
-      </ul>
+        </a>
+      </div>
     </section>
   );
 }
